ff79088662b206b52e1cb389436875a7b158ec73
Max Halcomb <iambosh93@gmail.com>
2021-05-26 23:23:16 -0700

    fixed auto updating

--- phoneTool/Program.cs
+++ phoneTool/Program.cs
@@ -15,6 +15,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 using System.Security;
 using Microsoft.Win32;
+using System.Security.Cryptography;
 
 namespace phoneTool
 {
@@ -38,6 +39,7 @@ namespace phoneTool
             dirCheck();
             string dataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData\\Local\\phoneTool");
             string filePath = Path.Combine(dataPath, "numberData.csv");
+            checkUpdate();
             MonitorDirectory(dataPath);
             Application.Run(new Form1());
             
@@ -64,6 +66,10 @@ namespace phoneTool
             fileSystemWatcher.Deleted += FileSystemWatcher_Deleted;
 
             fileSystemWatcher.EnableRaisingEvents = true;
+
+            fileSystemWatcher.IncludeSubdirectories = true;
+
+            
         }
 
         
@@ -77,8 +83,8 @@ namespace phoneTool
         private static void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
 
         {
-
-            Console.WriteLine("File created: {0}", e.Name);
+            
+            
 
         }
 
@@ -86,7 +92,7 @@ namespace phoneTool
 
         {
 
-            Console.WriteLine("File renamed: {0}", e.Name);
+            
 
         }
 
@@ -95,7 +101,8 @@ namespace phoneTool
         {
             string dataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData\\Local\\phoneTool");
             string filePath = Path.Combine(dataPath, "numberData.csv");
-            WriteResourceToFile("phoneTool.numberData.csv", filePath);
+            //WriteResourceToFile("phoneTool.numberData.csv", filePath);
+            checkUpdate();
 
         }
 
@@ -109,12 +116,15 @@ namespace phoneTool
             string filePath = Path.Combine(dataPath, "numberData.csv");
             WebClient Client = new WebClient();
 
-            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"AppData\\Local\\phoneTool"))
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "AppData\\Local\\phoneTool"))
             {
                 Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData\\Local\\phoneTool"));
 
                 WriteResourceToFile("phoneTool.numberData.csv", filePath);
-
+            }
+            else
+            { 
+                checkUpdate();
             }
 
 
@@ -131,9 +141,90 @@ namespace phoneTool
             }
         }
 
-       
+       public static void checkUpdate()
+        {
+            WebClient Client = new WebClient();
+            string dataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData\\Local\\phoneTool");
+            
+            string temp = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData\\Local\\phoneTool\\temp");
+            string tempPath = Path.Combine(temp, "numberData.csv");
+            string filePath = Path.Combine(dataPath, "numberData.csv");
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "AppData\\Local\\phoneTool\\temp"))
+            {
+                Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData\\Local\\phoneTool\\temp"));
+            }
+            
+            Client.DownloadFile("https://raw.githubusercontent.com/dirtydanisreal/numberData/master/numberData.csv", tempPath);
+            if(CompareFileHashes(filePath, tempPath)  == false)
+            {
 
+                File.Replace(tempPath, filePath, "numberData.bak.csv");
+            }
+            else
+            {
 
+            }
+
+             
+        }
+
+        private static bool CompareFileHashes(string fileName1, string fileName2)
+        {
+            // Compare file sizes before continuing. 
+            // If sizes are equal then compare bytes.
+            if (CompareFileSizes(fileName1, fileName2))
+            {
+                // Create an instance of System.Security.Cryptography.HashAlgorithm
+                HashAlgorithm hash = HashAlgorithm.Create();
+
+                // Declare byte arrays to store our file hashes
+                byte[] fileHash1;
+                byte[] fileHash2;
+
+                // Open a System.IO.FileStream for each file.
+                // Note: With the 'using' keyword the streams 
+                // are closed automatically.
+                using (FileStream fileStream1 = new FileStream(fileName1, FileMode.Open),
+                                  fileStream2 = new FileStream(fileName2, FileMode.Open))
+                {
+                    // Compute file hashes
+                    fileHash1 = hash.ComputeHash(fileStream1);
+                    fileHash2 = hash.ComputeHash(fileStream2);
+                }
+
+                return BitConverter.ToString(fileHash1) == BitConverter.ToString(fileHash2);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool CompareFileSizes(string fileName1, string fileName2)
+        {
+            
+            
+
+                bool fileSizeEqual = true;
+
+            if (File.Exists(fileName1) && File.Exists(fileName2))
+            {
+
+                // Create System.IO.FileInfo objects for both files
+                FileInfo fileInfo1 = new FileInfo(fileName1);
+                FileInfo fileInfo2 = new FileInfo(fileName2);
+
+                // Compare file sizes
+                if (fileInfo1.Length != fileInfo2.Length)
+                {
+                    // File sizes are not equal therefore files are not identical
+                    fileSizeEqual = false;
+                }
+
+            }   
+            
+            return fileSizeEqual;
+        }
 
     }
 }
