cde5eef3743d26d6933a2938b6beab5d28baae04
Max Halcomb <iambosh93@gmail.com>
2022-04-27 00:03:39 -0400

    Update Helper.cs

--- phoneTool/Helper.cs
+++ phoneTool/Helper.cs
@@ -153,6 +153,19 @@ namespace phoneTool
             
             return value;
         }
+        
+        public static bool isNumeric(string s)
+        {
+        foreach (char c in s)
+            {
+                if (!(c >= '0' && c <= '9')) 
+                {
+                return false;
+                }
+            }
+ 
+            return true;
+        }
 
 
     }
